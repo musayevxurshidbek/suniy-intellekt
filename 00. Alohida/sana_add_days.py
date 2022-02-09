84c7bccd04a2ef6a06584b678883f5a155b304a1
Xurshidbek <63257087+musayevxurshidbek@users.noreply.github.com>
2022-02-09 22:20:07 +0500

    To'g'rilandi

--- 00. Alohida/sana_add_days.py
+++ 00. Alohida/sana_add_days.py
@@ -11,19 +11,26 @@ if y % 400 == 0 or y % 4 == 0 and y % 100 != 0:
 else:
     kabisa = False
 
-while not (d <= 30 or d <= 31 or (d <= 29 and kabisa)):
+while d != 0:
     if y % 400 == 0 or y % 4 == 0 and y % 100 != 0:
         kabisa = True
     else:
         kabisa = False
+
     if m == 2:
-        if d > 28 + kabisa:
+        if d > (28 + kabisa):
             m += 1
-            d -= 28 + kabisa
+            d -= (28 + kabisa)
+        else:
+            q = d
+            d = 0
     elif m == 4 or m == 6 or m == 9 or m == 11:
         if d > 30:
             m += 1
             d -= 30
+        else:
+            q = d
+            d = 0
     else:
         if d > 31:
             m += 1
@@ -31,4 +38,7 @@ while not (d <= 30 or d <= 31 or (d <= 29 and kabisa)):
                 m = 1
                 y += 1
             d -= 31
-print(d, m, y, sep="/")
+        else:
+            q = d
+            d = 0
+print(q, m, y, sep="/")
